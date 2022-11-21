80156003f496ed8d42516dad4574a4bb65c0b649
VirginiaRool <virginia.aro@gmail.com>
2022-11-21 17:43:57 -0600

    Added Synonyms component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,17 +1,23 @@
 import React from "react";
 import "./Meaning.css";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
+  console.log(props.meaning.definition);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
+        console.log({ definition });
         return (
           <div key={index}>
             <p>
               {definition.definition}
               <br />
+              <strong>Example:</strong>
               <em>{definition.example}</em>
+
+              <Synonyms synonyms={props.meaning.synonyms} />
             </p>
           </div>
         );
