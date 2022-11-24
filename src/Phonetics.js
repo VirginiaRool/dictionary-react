08765fdde44cb075f443818457b7f7a9bb5d5dd6
VirginiaRool <virginia.aro@gmail.com>
2022-11-23 19:51:08 -0600

    Removed console.log phonetics

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./Phonetics.css";
 
 export default function Phonetis(props) {
-  console.log(props.phonetics);
   return (
     <div className="Phonetics">
       <a href={props.phonetics.audio} target="_blank" rel="noreferrer">
